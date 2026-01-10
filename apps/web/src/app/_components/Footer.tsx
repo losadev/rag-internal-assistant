c14e0192e84615a388d1ed95756e181b131bdade
losadev <losadauresdev@gmail.com>
2026-01-10 17:53:03 +0100

    Improves layout and footer with enhanced info and styling

--- apps/web/src/app/_components/Footer.tsx
+++ apps/web/src/app/_components/Footer.tsx
@@ -1,7 +1,8 @@
 export const Footer = () => {
   return (
-    <footer className="w-full text-center text-sm bg-gray-200 border-gray-400 text-gray-500 border-t py-8">
-      Internal demo app - Next.js + LangChain + MCP + n8n
+    <footer className="w-full text-center text-sm bg-gray-200 border-gray-400 text-gray-500 border-t py-8 leading-7 bottom-0 absolute">
+      Internal demo app - Next.js + LangChain + MCP + n8n by <br />
+      <strong>Pablo Losada</strong> &#169; {new Date().getFullYear()}
     </footer>
   );
 };
--- apps/web/src/app/upload/page.tsx
+++ apps/web/src/app/upload/page.tsx
@@ -1,8 +1,10 @@
 export default function UploadPage() {
-    return (
-        <div>
-            <h1>Upload Page</h1>
-            <p>¡La página funciona correctamente!</p>
-        </div>
-    );
+  return (
+    <div className="w-full h-full p-6">
+      <h1 className="text-4xl font-bold text-gray-800">Upload Page</h1>
+      <p className="text-lg text-gray-600 mt-4">
+        ¡La página funciona correctamente!
+      </p>
+    </div>
+  );
 }
